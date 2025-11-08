a3c822a5df4621a092ef397e39a93b462c6bfde2
Storfiskur <dthrll@gmail.com>
2025-11-08 21:50:39 +0500

    adding svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue(), svelte()]
 });
---
+++ src/components/MySvelteComp.svelte
@@ -0,0 +1,12 @@
+<script>
+	let count = 0;
+
+	function increment() {
+		count++
+	}
+</script>
+
+<button onclick={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
