1cd3a535970c4299c9c76d353dff26ef47d58a0c
Storfiskur <dthrll@gmail.com>
2025-11-08 14:37:39 +0500

    added island

--- src/components/MyComp.vue
+++ src/components/MyComp.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { ref } from "vue";
+
+const myVar = ref(0);
+</script>
+
 <template>
-  <h2>Привет</h2>
+  <h2>{{ myVar }}</h2>
+  <button @click="myVar++">pushme</button>
 </template>
