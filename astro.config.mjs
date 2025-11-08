903473ed6473729f6b594d08cb530b1474866243
Storfiskur <dthrll@gmail.com>
2025-11-09 00:03:09 +0500

    removed svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import vue from '@astrojs/vue';
+import vue from "@astrojs/vue";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), svelte()]
+  integrations: [vue()],
 });
