5d349863a0ae515a7e44fe12097781cfd53dee9b
Storfiskur <dthrll@gmail.com>
2025-11-10 18:28:12 +0500

    redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from "astro/config";
 
 import vue from "@astrojs/vue";
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
